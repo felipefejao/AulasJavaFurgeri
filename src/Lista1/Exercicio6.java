e41e15ba924abb3e660765b6d377019d74140772
felipefejao <felipe.silva@uso.com.br>
2018-08-17 18:50:55 -0300

    Até o exercício 6 finalizado

--- src/Lista1/Exercicio6.java
+++ src/Lista1/Exercicio6.java
@@ -1,8 +1,17 @@
 package Lista1;
 
+import javax.swing.JOptionPane;
+
 public class Exercicio6 {
 	public static void main(String[] args) {
-		Number n1 = Util.getInput("Numero: ");
+		String n1 = JOptionPane.showInputDialog("Digite um número");
 		
+		try {
+			int conv = Integer.parseInt(n1);
+			
+			JOptionPane.showMessageDialog(null, "É inteiro");
+		}catch (NumberFormatException e) {
+			JOptionPane.showMessageDialog(null, "Não é inteiro");
+		}
 	}
 }
